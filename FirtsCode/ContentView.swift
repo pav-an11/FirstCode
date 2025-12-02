a4910f6951d58047c17a3e8ee1cdfb8f474383f0
Heath-280 <heathkumar123@gmail.com>
2025-12-02 16:13:12 +0530

    Initial Commit

---
+++ FirtsCode/ContentView.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Swift Coding Club")
+                .font(.system(size: 20, weight:  .bold))
+                .foregroundColor(.red)
+            Button(action: {
+                
+            }){
+                Text("Button")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding(8)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.black)
+                    .cornerRadius(10)
+            }
+        }
+        .padding(60)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FirtsCode/FirtsCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirtsCodeApp.swift
+//  FirtsCode
+//
+//  Created by student on 02/12/25.
+//
+
+import SwiftUI
+
+@main
+struct FirtsCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
